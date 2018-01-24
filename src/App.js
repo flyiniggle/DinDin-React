1b80309d69e8f221f821a8a7b9122efcbc5bab98
Dan Thompson <dthompson@vestmark.com>
2018-01-24 16:01:40 -0500

    grrrrrr

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
 
-class App extends Component {
-  render() {
+import Toolbar from './Toolbar/Toolbar';
+import Home from './Home/Home';
+import'../node_modules/bootstrap/less/bootstrap.less'
+
+const App = function() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    	<div className="container-fluid">
+			<Toolbar />
+			<BrowserRouter>
+				<Route path='/' component={Home} />
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
