8445a78f12057fd0678a129920bcf63ee2998973
Dan Thompson <dthompson@vestmark.com>
2018-01-25 10:25:30 -0500

    updates

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Home = function() {
-	return <div>HOME!</div>
+const Home = function(props) {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-xs-12">
+					<p>filter controls</p>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-xs-12 col-md-4">
+					<p>overview!</p>
+				</div>
+				<div className="col-xs-12 col-md-8">
+					<p>meal cards!</p>
+				</div>
+			</div>
+		</div>
+	)
 };
 
 export default Home;
